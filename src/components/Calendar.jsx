ee5326811c08171cc76132a92d3c4a95dd32660b
aakash-22ak <akashshrestha678@gmail.com>
2023-09-27 13:48:19 +0545

    created the calendar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,6 +1,77 @@
 export default function Calendar() {
-  return;
-  <div>
-    <h1>hello </h1>
-  </div>;
+  const days = ["आइत", "सोम", "मंगल", "बुध", "बिहि", "शुक्र", "शनि "];
+  const date = [
+    "१",
+    "२",
+    "३",
+    "४",
+    "५",
+    "६",
+    "७",
+    "८",
+    "९",
+    "१०",
+    "११",
+    "१२",
+    "१३",
+    "१४",
+    "१५",
+    "१६",
+    "१७",
+    "१८",
+    "१९",
+    "२०",
+    "२१",
+    "२२",
+    "२३",
+    "२४",
+    "२५",
+    "२६",
+    "२७",
+    "२८",
+    "२९",
+    "३०",
+  ];
+
+  return (
+    <div className="flex justify-between  items-center min-h-screen m-5 ">
+      {/*header divisioasn */}
+      <div className="w-[800px] h-[500px]  relative shadow-lg overflow-hidden">
+        <div className="flex justify-between bg-red-600 text-center px-5 py-5">
+          <h1 className="text-2xl text-white">२०८०</h1>
+          <h1 className="text-2xl text-white">असोज </h1>
+        </div>
+        <div className="grid grid-cols-7 bg-slate-100 gap-4 pt-5 ">
+          {days.map((day, index) => {
+            return (
+              <div key={index} className=" p-2 text-center">
+                {day}
+              </div>
+            );
+          })}
+        </div>
+        <div className="grid grid-cols-7 ">
+          {date.map((item, index) => {
+            const isEven = index % 2 === 0;
+            return (
+              <>
+                {isEven ? (
+                  <div key={index} className="bg-pink-300 p-6 text-center">
+                    {item}
+                  </div>
+                ) : (
+                  <div key={index} className="bg-pink-400 p-6 text-center">
+                    {item}
+                  </div>
+                )}
+              </>
+            );
+          })}
+        </div>
+      </div>
+      <div>
+        <main>Events</main>
+      </div>
+    </div>
+  );
 }
