71c3cdc7aa05d6ce9bceca833eea313aac1a6932
RoshanDalami <roshandalami0@gmail.com>
2023-09-28 11:24:47 +0545

    added model

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,18 @@
+'use client'
+
+
 import CalendarTwo from "@/components/CalendarTwo";
+import Calendar from "../components/Calendar";
 import Image from "next/image";
 
 export default function Home() {
+
+  
+
   return (
-
-
       
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CalendarTwo />
+      {/* <CalendarTwo /> */}
     <Calendar />
     </main>
   );
